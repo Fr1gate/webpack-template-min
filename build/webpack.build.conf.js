a9b2da8d4087d138afa6ae5ccd6bcbb244d5fef6
Dmitriy <pershikovdmitriy@yandex.ru>
2020-10-27 09:03:48 +0300

    Update webpack.build.conf.js

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,7 +1,11 @@
 const {merge} = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    publicPath: './',
+    output: {
+        filename: `${PATHS.assets}/js/[name].js`,
+        path: PATHS.dist,
+        publicPath: './'
+    },
     mode: "production",
     plugins: []
 })
